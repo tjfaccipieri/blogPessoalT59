8c5d7d5bd647f5508efab41cd5027c2a766f169b
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2023-03-01 07:40:43 -0300

    Navbar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Home from './components/home/Home';
+import Home from './paginas/home/Home';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
 
   return (
     <div>
+      <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
